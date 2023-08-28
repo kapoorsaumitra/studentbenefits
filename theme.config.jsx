04f1e5bec6928977f016ce955cd6b17ea42b86bc
kapoorsaumitra <saumitrakapoor24@gmail.com>
2023-08-28 22:50:37 +0530

    Initial Project Setup

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <strong>Student Benefits</strong>,
+    project: {
+      link: 'https://github.com/kapoorsaumitra/studentbenefits'
+    },
+    docsRepositoryBase:'https://github.com/kapoorsaumitra/studentbenefits/tree/main/'
+  }
