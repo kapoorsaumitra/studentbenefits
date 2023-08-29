7a96ab72deaa410a5cbca6707e3a0519c182f8cd
kapoorsaumitra <saumitrakapoor24@gmail.com>
2023-08-29 15:38:14 +0530

    chore: Update Multiple Files

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <strong>Student Benefits</strong>,
+  project: {
+    link: 'https://github.com/kapoorsaumitra/studentbenefits'
+  },
+  docsRepositoryBase:'https://github.com/kapoorsaumitra/studentbenefits/tree/main/',
+  darkMode : true,
+  primaryHue: Math.round(Math.random() * 360)
+}
+
+export default config
